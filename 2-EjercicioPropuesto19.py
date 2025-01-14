ec2ca283a7ea47c312fdf56c9c42278f5b9df01c
MiguelRendonG <mirendong@unal.edu.co>
2025-01-13 22:28:26 -0500

    Update 2-EjercicioPropuesto19.py

--- 2-EjercicioPropuesto19.py
+++ 2-EjercicioPropuesto19.py
@@ -21,27 +21,26 @@ class App:
         self.root = root
         self.root.title("Cálculos de Triángulo Equilátero")
 
-        # Etiqueta y entrada para el lado
+
         tk.Label(root, text="Valor del lado:").grid(row=0, column=0, padx=10, pady=5)
         self.lado_entry = tk.Entry(root)
         self.lado_entry.grid(row=0, column=1, padx=10, pady=5)
 
-        # Botón para calcular
+   
         self.calcular_button = tk.Button(root, text="Calcular", command=self.calcular)
         self.calcular_button.grid(row=1, column=0, columnspan=2, pady=10)
 
     def calcular(self):
         try:
-            # Captura de datos
+     
             lado = float(self.lado_entry.get())
 
-            # Crear objeto TrianguloEquilatero y calcular resultados
+     
             triangulo = TrianguloEquilatero(lado)
             perimetro = triangulo.calcular_perimetro()
             altura = triangulo.calcular_altura()
             area = triangulo.calcular_area()
 
-            # Mostrar resultados
             messagebox.showinfo("Resultados", f"Lado: {lado}\n"
                                             f"Perímetro: {perimetro:.2f}\n"
                                             f"Altura: {altura:.2f}\n"
